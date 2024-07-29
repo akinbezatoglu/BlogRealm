5a33add8af27add6a7a6313b80acac84dc5433fe
akinbezatoglu <akinbezatoglu@gmail.com>
2024-07-29 10:28:53 +0300

    feat: Implement dependency injection in Web layer and enhance Core, Data, and Services layers
    https://gist.github.com/davidfowl/563a602936426a18f67cd77088574e61

---
+++ BlogRealm.Web/App_Start/ScopedDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogRealm.Web.App_Start
+{
+    public class ScopedDependencyResolver : IDependencyResolver
+    {
+        private readonly IServiceProvider _serviceProviderRoot;
+
+        public ScopedDependencyResolver(IServiceProvider serviceProviderRoot)
+        {
+            _serviceProviderRoot = serviceProviderRoot;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return resolveServiceProvider().GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return resolveServiceProvider().GetServices(serviceType);
+        }
+
+        private IServiceProvider resolveServiceProvider()
+        {
+            if (HttpContext.Current == null)
+            {
+                Debug.WriteLine("Resolving services from root container !");
+                return _serviceProviderRoot;
+            }
+
+            var scope = HttpContext.Current.Items[typeof(IServiceScope)] as IServiceScope;
+
+            if (scope == null)
+            {
+                scope = _serviceProviderRoot.CreateScope();
+                HttpContext.Current.Items[typeof(IServiceScope)] = scope;
+            }
+            return scope.ServiceProvider;
+        }
+    }
+}
--- BlogRealm.Web/Global.asax.cs
+++ BlogRealm.Web/Global.asax.cs
@@ -1,21 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AutoMapper;
+using BlogRealm.Core;
+using BlogRealm.Core.Services;
+using BlogRealm.Data;
+using BlogRealm.Data.Context;
+using BlogRealm.Services;
+using BlogRealm.Web;
+using BlogRealm.Web.App_Start;
+using BlogRealm.Web.Controllers;
+using Microsoft.Extensions.DependencyInjection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+[assembly: PreApplicationStartMethod(typeof(MvcApplication), "InitModules")]
+
 namespace BlogRealm.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void InitModules()
+        {
+            RegisterModule(typeof(ScopedDependencyHttpModule));
+        }
+
         protected void Application_Start()
         {
+            ConfigureServices();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<BlogRealmDbContext>();
+
+            services.AddTransient<IAboutService, AboutService>();
+            services.AddTransient<IAuthorService, AuthorService>();
+            services.AddTransient<IBlogService, BlogService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IContactService, ContactService>();
+            services.AddTransient<INewsletterService, NewsletterService>();
+            services.AddTransient<ISocialMediaAccountService, SocialMediaAccountService>();
+            services.AddTransient<ISocialMediaPlatformService, SocialMediaPlatformService>();
+            services.AddTransient<IUserService, UserService>();
+
+            var rootProvider = services.BuildServiceProvider(true);
+            var resolver = new ScopedDependencyResolver(rootProvider);
+            DependencyResolver.SetResolver(resolver);
         }
     }
 }
