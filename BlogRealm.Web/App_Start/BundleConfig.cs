69d8565e267148d907e524f31c0d4e4cac94dd53
akinbezatoglu <akinbezatoglu@gmail.com>
2024-07-31 12:52:00 +0300

    Add Blogy website template to Web layer

--- BlogRealm.Web/App_Start/BundleConfig.cs
+++ BlogRealm.Web/App_Start/BundleConfig.cs
@@ -20,11 +20,40 @@ namespace BlogRealm.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/blogrealmjs").Include(
+                      "~/Scripts/tiny-slider.js",
+                      "~/Scripts/flatpickr.min.js",
+                      "~/Scripts/aos.js",
+                      "~/Scripts/glightbox.min.js",
+                      "~/Scripts/navbar.js",
+                      "~/Scripts/counter.js",
+                      "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-grid.css",
+                      "~/Content/bootstrap-reboot.css",
+                      "~/Content/bootstrap-utilities.css",
+                      "~/Content/aos.css",
+                      "~/Content/flaticon.css",
+                      "~/Content/icomoon.css",
+                      "~/Content/tiny-slider.css",
+                      "~/Content/glightbox.min.css",
+                      "~/Content/flatpickr.min.css",
+                      "~/Content/site.css",
+                      "~/Content/fonts/flaticon/font/flaticon.css",
+                      "~/Content/fonts/flaticon/font/Flaticon.eot",
+                      "~/Content/fonts/flaticon/font/Flaticon.ttf",
+                      "~/Content/fonts/flaticon/font/Flaticon.woff",
+                      "~/Content/fonts/flaticon/font/Flaticon.woff2",
+                      "~/Content/fonts/icomoon/icomoon.css",
+                      "~/Content/fonts/icomoon/selection.json",
+                      "~/Content/fonts/icomoon/fonts/icomoon.eot",
+                      "~/Content/fonts/icomoon/fonts/icomoon.ttf",
+                      "~/Content/fonts/icomoon/fonts/icomoon.woff"));
         }
     }
 }
